5f9ebffe6c2850872b426e2060e26e7e3dce04a6
Rohit Barma <rohit@example.com>
2025-07-17 15:25:10 +0530

    Fix vite config and re-deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <div className='min-h-screen bg-gray-200 flex flex-col items-center justify-center'>
-=======
-      <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
->>>>>>> d4bf5fc89fc86c3c1b1ae6f1a3381f073aecfa43
         <CurrencyConverter/>
       </div>
     </>
--- src/components/Dropdowns.jsx
+++ src/components/Dropdowns.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Dropdowns = ({currencies, currency, setCurrency, title=""}) => {
   return (
     <div>
@@ -24,30 +24,3 @@ const Dropdowns = ({currencies, currency, setCurrency, title=""}) => {
 }
 
 export default Dropdowns
-=======
-const Dropdowns = ({currencies, currency, setCurrency, title=""}) => {
-  return (
-    <div>
-        <label htmlFor={title} className='block text-sm font-medium text-gray-700 mb-[3px]'> {title} </label>
-        <div className='relative'>
-            <select 
-                onChange={(e) => setCurrency(e.target.value)} 
-                value={currency} 
-                className='w-full bg-gray-200 font-semibold p-2 border rounded-md border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 cursor-pointer'
-            >
-                 
-                <hr />
-                {currencies.map((currency) => {
-                    return(<option value={currency} key={currency}>
-                        {currency}
-                    </option>)
-
-                })}
-            </select> 
-        </div>
-    </div>
-  )
-}
-
-export default Dropdowns
->>>>>>> d4bf5fc89fc86c3c1b1ae6f1a3381f073aecfa43
